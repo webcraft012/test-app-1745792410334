846e33e2af74bf00909ade3f0ddb29decdd090c6
CodeSandbox <support@codesandbox.io>
2025-04-27 22:24:29 +0000

    Create file:components/TodoList.tsx

---
+++ components/TodoList.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+   import { useEffect } from 'react';
+   import { useTodoStore } from '@/lib/store/todo-store';
+   import { TodoItem } from '@/components/TodoItem';
+
+   export function TodoList() {
+     const { todos, loading, error, fetchTodos } = useTodoStore();
+
+     useEffect(() => {
+       fetchTodos();
+     }, [fetchTodos]);
+
+     if (loading) {
+       return <div>Loading todos...</div>;
+     }
+
+     if (error) {
+       return <div className="text-red-500">Error: {error}</div>;
+     }
+
+     if (todos.length === 0) {
+       return <div>No todos yet. Add one above!</div>;
+     }
+
+     return (
+       <div className="space-y-2">
+         {todos.map((todo) => (
+           <TodoItem key={todo.id} todo={todo} />
+         ))}
+       </div>
+     );
+   }
